5dcc92d1ce5e52b6538175dc0284ed29a801c484
BixelPitch <hannes@bixelspace.de>
2020-12-22 13:52:55 +0100

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>CAH Cards</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>Cards Against Humanity</h1>
+        <h2>Work in progress!</h2>
+      </main>
+
+      <footer>
+      </footer>
+    </div>
+  );
+}
